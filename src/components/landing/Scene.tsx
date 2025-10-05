77af5c4cc1f0738be98d011c270eb591f49741e1
Adarsh Chaudhary <adarsh44532@gmail.com>
2025-10-05 08:39:22 +0000

    https://prod.spline.design/gDJ5QLEr1iRswFIj/scene.splinecode   import Sp

--- src/components/landing/Scene.tsx
+++ src/components/landing/Scene.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline/next';
 
 export default function Scene() {
   return (
-    <Spline scene="https://prod.spline.design/N-3O04w1u2gWBrSt/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/gDJ5QLEr1iRswFIj/scene.splinecode" />
   );
 }
