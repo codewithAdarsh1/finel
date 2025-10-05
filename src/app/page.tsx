6723d103138ebca5c9beec6e0bf4c9d9f6538cc3
Adarsh Chaudhary <adarsh44532@gmail.com>
2025-10-05 07:13:31 +0000

    use the design concept i mean the grid left and right ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
 import type { LatLng } from 'leaflet';
 import AirQualityReport from '@/components/map/air-quality-report';
 import MapView from '@/components/map/map-view';
